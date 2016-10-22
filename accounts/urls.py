77a8807a7ac867599b575a08a6c8229c1ac1cb1c
Bekzat <bekzat.95@mail.ru>
2016-10-23 01:59:31 +0700

    Подправил логин и регистрацию. При логине редайректит на нужные страницы ( админ - админ страница, саундман - саундман страница) Добавил соответственно данные страницы

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from accounts import views
 
 urlpatterns = [
     url(r'^login', views.login, name='login'),
     url(r'^logout', views.logout, name='logout'),
     url(r'^register', views.register, name="reg"),
+    url(r'^administrator_page', views.administrators_page, name='adm'),
+    url(r'^soundman', views.soundman_page, name='sndmn')
 ]
