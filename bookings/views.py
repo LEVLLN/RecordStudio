4c7b67c303271d6a0d5efd7da00eed52b9d3760a
TRPO6 <lln-nz@mail.ru>
2016-10-22 21:46:19 +0700

    Method 'create_booking' realized toDo: create Soundman picker

--- bookings/views.py
+++ bookings/views.py
@@ -6,12 +6,12 @@ from bookings.models import Reservation
 import datetime
 from pytimeparse.timeparse import timeparse
 
+
 def home(request):
-    return render(request, 'bookings/home.html')
+    return render(request, 'bookings/create_booking.html')
+
 
 def create_booking(request):
-
-
     duration = request.POST['duration']
     start = request.POST['start']
 
@@ -19,10 +19,12 @@ def create_booking(request):
     print(duration)
 
     user = request.user
-    new_booking = Reservation(user=user, start=start, is_active=1, duration=datetime.timedelta(minutes=timeparse(duration)), soundman=user)
+    new_booking = Reservation(user=user, start=start, is_active=1,
+                              duration=datetime.timedelta(minutes=timeparse(duration)),
+                              soundman=user)
     new_booking.save()
     reserv = Reservation.objects.all()
     context = {
         'bookings': new_booking
     }
-    return render(request, 'bookings/create_booking.html', context)
+    return render(request, 'bookings/show_booking.html', context)
