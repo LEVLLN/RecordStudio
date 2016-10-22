d04538c2ac3e8d8b817cc8ea299ac89ed9a2a028
Bekzat <bekzat.95@mail.ru>
2016-10-22 19:03:49 +0700

    реализованы Авторизация и регистрация

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from bookings import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^', views.home, name='home'),
+    url(r'^book/', views.booking, name='booking'),
+    url(r'^test/', views.test, name='test'),
 
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required()
 def home(request):
-    return HttpResponse('cfsdfdsf')
+    return render(request, 'bookings/home.html')
+
+
+def test(request):
+    return HttpResponse(request, "fdsfdsfs")
+
+
+def booking(request):
+    template = "booking/book.html"
+    return HttpResponse(request, template)
