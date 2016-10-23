843789b5b5fb0a17254a8d5b1a7577d4fa7495f9
TRPO6 <lln-nz@mail.ru>
2016-10-23 07:18:24 +0700

    Объединил методы во bookings/view.py create_booking и home(их закомментировал) в один метод "creating_booking" с условием на пост и гет запросы, редирект на два разных html файла (не уверен,что верное решение для такого метода. обсудим)

--- bookings/urls.py
+++ bookings/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from bookings import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^create_booking',views.create_booking,name='create_booking')
+    url(r'^creating_booking', views.creating_booking, name='creating_booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,38 +1,64 @@
 from django.contrib.auth.models import User, Group
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import auth
+from django.template.context_processors import csrf
+
 from bookings.models import Reservation
 import datetime
 from pytimeparse.timeparse import timeparse
 
 
-def home(request):
-    # soundman = User.objects.filter(groups='soundman')
+# def home(request):
+#     # soundman = User.objects.filter(groups='soundman')
+#
+#     group = Group.objects.get(name='soundmans')
+#     users = group.user_set.all()
+#     context = {
+#         'sounmanlist': users
+#     }
+#     print(users)
+#     return render(request, 'bookings/create_booking.html', context)
+#
 
-    group = Group.objects.get(name='soundmans')
-    users = group.user_set.all()
-    context = {
-        'sounmanlist': users
-    }
-    print(users)
-    return render(request, 'bookings/create_booking.html',context)
+# def create_booking(request):
+#     duration = request.POST['duration']
+#     start = request.POST['start']
+#     soundman = request.POST['soundman']
+#     print(start)
+#     print(soundman)
+#
+#     user = request.user
+#     new_booking = Reservation(user=user, start=start, is_active=1,
+#                               duration=datetime.timedelta(minutes=timeparse(duration)),
+#                               soundman=request.user)
+#     new_booking.save()
+#     reserv = Reservation.objects.all()
+#     context = {
+#         'bookings': new_booking
+#     }
+#     return render(request, 'bookings/show_booking.html', context)
 
 
-def create_booking(request):
-    duration = request.POST['duration']
-    start = request.POST['start']
-    soundman = request.POST['soundman']
-    print(start)
-    print(soundman)
+def creating_booking(request):
+    args = {}
+    args.update(csrf(request))
+    if request.method=="GET":
+        group = Group.objects.get(name='soundmans')
+        users = group.user_set.all()
+        context = {
+            'sounmanlist': users
+        }
+        print(users)
+        return render(request, 'bookings/create_booking.html', context)
+    elif request.method=="POST":
+        duration = request.POST['duration']
+        start = request.POST['start']
+        soundman = request.POST['soundman']
+        user = request.user
+        new_booking = Reservation(user=user, start=start, is_active=1,
+                                  duration=datetime.timedelta(minutes=timeparse(duration)),
+                                  soundman=request.user)
+        new_booking.save()
+        return render(request, 'bookings/show_booking.html', {'booking': new_booking})
 
-    user = request.user
-    new_booking = Reservation(user=user, start=start, is_active=1,
-                              duration=datetime.timedelta(minutes=timeparse(duration)),
-                              soundman=request.user)
-    new_booking.save()
-    reserv = Reservation.objects.all()
-    context = {
-        'bookings': new_booking
-    }
-    return render(request, 'bookings/show_booking.html', context)
