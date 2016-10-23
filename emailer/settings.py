ff05c28f971937a35e6bbe09d958d8e278270b3e
Bekzat <bekzat.95@mail.ru>
2016-10-24 01:23:35 +0700

    сорян, папку это забыл добавить

---
+++ emailer/settings.py
@@ -0,0 +1,8 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sbekzatm@gmail.com'
+DEFAULT_FROM_EMAIL = 'sbekzatm@gmail.com'
+SERVER_EMAIL = 'sbekzatm@gmail.com'
+EMAIL_HOST_PASSWORD = '9ib10wy04B'
