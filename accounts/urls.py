efc7ec2d383c98f499655a584d75a44fc57e4a33
Bekzat <bekzat.95@mail.ru>
2016-10-25 13:24:09 +0700

    test

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from accounts import views
 
 urlpatterns = [
+    # dsfdsdsfdsf
     url(r'^login', views.login, name='login'),
     url(r'^logout', views.logout, name='logout'),
     url(r'^register', views.register, name="reg"),
