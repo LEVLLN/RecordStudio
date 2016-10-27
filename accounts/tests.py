2e49c3805496507e7cba7592d98648838329680f
Bekzat <bekzat.95@mail.ru>
2016-10-27 23:23:26 +0700

    testing tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import unittest
 
-# Create your tests here.
+
+class LoginTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def login_get(self):
+        response = self.client.get('/accounts/login')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'login.html')
+
+    def login_post(self):
+        response = self.client.post('/accounts/login', {'username':'admin', 'password':'qwertY123'})
+        self.assertEquals(response.status_code, 200)
+
+
+class RegisterTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_register_get(self):
+        response = self.client.get('/accounts/register')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_post(self):
+        response = self.client.post('/accounts/register', {'first_name':'Bekzat', 'last_name':'Shayakhmetov',
+                                                        'username':'smbkzt', 'password1':'2251452Bb', 'password2':"2251452Bb"})
+        self.assertEqual(response.status_code, 200)
