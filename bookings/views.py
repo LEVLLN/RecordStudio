6c8feb5625debcc3fc04ff2952f91d4e524a0f87
Zhanserik <zhanserik.baikadir@gmail.com>
2016-11-19 14:15:35 +0700

    Removed unused definitions and refactoring

--- bookings/views.py
+++ bookings/views.py
@@ -12,65 +12,6 @@ from pytimeparse.timeparse import timeparse
 from bookings.models import Booking, Schedule, Record
 
 
-# def home(request):
-#     # soundman = User.objects.filter(groups='soundman')
-#
-#     group = Group.objects.get(name='soundmans')
-#     users = group.user_set.all()
-#     context = {
-#         'sounmanlist': users
-#     }
-#     print(users)
-#     return render(request, 'bookings/create_booking.html', context)
-#
-
-# def create_booking(request):
-#     duration = request.POST['duration']
-#     start = request.POST['start']
-#     soundman = request.POST['soundman']
-#     print(start)
-#     print(soundman)
-#
-#     user = request.user
-#     new_booking = Reservation(user=user, start=start, is_active=1,
-#                               duration=datetime.timedelta(minutes=timeparse(duration)),
-#                               soundman=request.user)
-#     new_booking.save()
-#     reserv = Reservation.objects.all()
-#     context = {
-#         'bookings': new_booking
-#     }
-#     return render(request, 'bookings/show_booking.html', context)
-
-
-# ___________________________________________________________________________________________
-def creating_booking(request):
-    args = {}
-    args.update(csrf(request))
-    if request.method == "GET":
-        group = Group.objects.get(name='Soundmans')
-        users = group.user_set.all()
-        context = {
-            'sounmanlist': users
-        }
-        print(users)
-        return render(request, 'bookings/create_booking.html', context)
-    elif request.method == "POST":
-        duration = request.POST['duration']
-        start = request.POST['start']
-        soundman_str = request.POST['soundman']
-        soundman = User.objects.get(username=soundman_str)
-        user = request.user
-        new_booking = Booking(user=user, start=start, is_active=1,
-                              duration=datetime.timedelta(minutes=timeparse(duration)),
-                              soundman=soundman)
-        if request.user.groups.filter(name='Customers').exists():
-            new_booking.save()
-            return render(request, 'bookings/show_booking.html', {'booking': new_booking})
-        else:
-            return HttpResponse('U cant do this')
-
-
 def home(request):
     return render(request, "bookings/home.html")
 
@@ -79,8 +20,6 @@ def about(request):
     return render(request, "bookings/about.html")
 
 
-# _________________________________________________________________________________________
-#
 def show_soundmans(request):
     group = Group.objects.get(name="Soundmans")
     soundmans = group.user_set.all()
@@ -160,52 +99,6 @@ def create_booking(request, soundman_id):
 
         return render(request, 'bookings/show_result.html', {'new_booking': new_booking})
 
-
-
-# def show_soundman_schedule(request, soundman_id):
-#  soundman = get_object_or_404(User, id=soundman_id)
-#     print(soundman)
-#     schedules = Schedule.objects.all().filter(soundman=soundman)
-#     print(schedules)
-#     bookings = Booking.objects.all().filter(soundman=soundman)
-#     context = {
-#         'schedules': schedules,
-#         'bookings': bookings
-#     }
-#     for sch in schedules:
-#         deltaStart = timedelta(hours=sch.start_of_the_day.hour, minutes=sch.start_of_the_day.minute)
-#         deltaEnd = timedelta(hours=sch.end_of_the_day.hour, minutes=sch.end_of_the_day.minute)
-#         delta = deltaEnd - deltaStart
-#         slotscount = (delta.seconds / 3600) / 2
-#         slotstimeStart = deltaStart
-#         # for i in slotscount:
-#         #     slotstimeEnd = slotstimeStart+2
-#         #     slotsStart.insert(slotstimeStart)
-#         #     print(slotstimeStart)
-#         k = int(slotscount)
-#         i = 0
-#         slots = []
-#         starEndslots = []
-#         print("Начало расписания:",sch.start_of_the_day)
-#         print("Конец расписания:", sch.end_of_the_day)
-#         print("Всего слотов на это расписание:", k)
-#         while i < k:
-#             i = i + 1
-#             print("Слот номер:",i)
-#             print("Начало времени слота:",slotstimeStart)
-#             slotstimeEnd = slotstimeStart + timedelta(hours=2)
-#             print("Конец времени слота:",slotstimeEnd)
-#             starEndslots = [(slotstimeStart.seconds/3600), slotstimeEnd.seconds/3600]
-#             slots.insert(i, starEndslots)
-#             slotstimeStart = slotstimeEnd
-#             # print(slotstimeStart)
-#             # slottimeEnd = slotstimeStart+2
-#             # slotstimeStart = slottimeEnd
-#             # print(slottimeEnd)
-#
-#             # check git cmd changes
-#         print("непонятный список слотов:",slots)
-#     return render(request, "bookings/show_schedule.html", context)
 
 class CurrentRecordsView:
     def get(self):
