3fa1dc2977ea5d0fb808485578d131939be4a4f2
Zhanserik <zhanserik.baikadir@gmail.com>
2016-11-26 21:07:19 +0700

    Styling for successfull page

--- emailer/views.py
+++ emailer/views.py
@@ -13,7 +13,7 @@ def send_email(**kwargs):
         username = kwargs['username']
         __hash_code = kwargs['hash_code']
         message = "http://127.0.0.1:8000/accounts/confirm?username=%s&hash=%s" % (username, __hash_code)
-        args['register'] = "Вы успешно зарегестрировались на сайте. Осталось подтвердить пароль"
+        args['register'] = "Вы успешно зарегестрировались на сайте. Осталось подтвердить email"
 
     # resend email
     elif len(kwargs) == 3 and 'hash_code' in kwargs.keys():
@@ -29,11 +29,11 @@ def send_email(**kwargs):
         email = kwargs['email']
         password = kwargs['password']
         message = "Your data: " + username + " " + password
-        args['forget'] = "Ваш новый пароль успешно отправлены на Ваш имейл"
+        args['forget'] = "Ваш новый пароль успешно отправлен на Ваш email"
 
     try:
         mail.send_mail('Subject here', message, settings.EMAIL_HOST_USER, [email], fail_silently=False)
         return render_to_response("emailer/successful_page.html", args)
     except Exception:
-        args['email_error'] = "Error while sending an email"
+        args['email_error'] = "Ошибка при отправке письма на email"
         return render_to_response("emailer/successful_page.html", args)
