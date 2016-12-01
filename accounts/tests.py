d9f119ca43b84ba653d4b3eba81e88cce7bdee7f
Bekzat <bekzat.95@mail.ru>
2016-12-01 17:07:53 +0700

    tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,21 +1,20 @@
-from django.test import TestCase, Client
-from django.contrib.auth import authenticate
-
-from .forms import UserCreationForm
 import unittest
+
+from django.test import Client
+
+from .forms import UserCreationForm, SoundmanCreationForm
 
 
 class LoginTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
 
-    def login_get(self):
+    def test_login_get(self):
         response = self.client.get('/accounts/login')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, 'login.html')
 
-    def login_post(self):
-        response = self.client.post('/accounts/login', {'username':'admin', 'password':'qwertY123'})
+    def test_login_post(self):
+        response = self.client.post('/accounts/login', {'username': 'tester', 'password': 'Jaxonyo23'})
         self.assertEquals(response.status_code, 200)
 
 
@@ -24,8 +23,9 @@ class RegisterTest(unittest.TestCase):
         self.client = Client()
 
     def test_form(self):
-        form_data = {'first_name':'Bekzat', 'last_name':'Shayakhmetov',
-                                    'username':'smbkzt', 'password1':'2251452Bb', 'password2':'2251452Bb', 'email':'bekzat.98@mail.ru'}
+        form_data = {'first_name': 'Bekzat', 'last_name': 'Shayakhmetov',
+                     'username': 'smbkzt', 'password1': '2251452Bb', 'password2': '2251452Bb',
+                     'email': 'bekzat.98@mail.ru'}
         form = UserCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
 
@@ -33,22 +33,41 @@ class RegisterTest(unittest.TestCase):
         response = self.client.get('/accounts/register')
         self.assertEqual(response.status_code, 200)
 
-    # def test_register_post(self):
-    #     data = {'first_name':'Bekzat', 'last_name':'Shayakhmetov','username':'smbkzt', 'password1':'2251452Bb', 'password2':'2251452Bb', 'email':'bekzat.98@mail.ru'}
-    #     response = self.client.post('/accounts/register', data)
-    #     self.client.login(username='smbkzt', password='2251452Bb')
-    #     self.assertEqual(response.status_code, 200)
+
+class SoundmanAddTet(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_form(self):
+        form_data = {'first_name': 'Bekzat', 'last_name': 'Shayakhmetov',
+                     'username': "smbkzt", "email": "smbekzat@hotmail.com"}
+        form = SoundmanCreationForm(data=form_data)
+        self.assertTrue(form.is_valid())
 
 
-# class EmailTest(TestCase):
-#     def test_send_email(self):
-#         mail.send_mail(
-#             'Subject here', 'Here is the message.',
-#             'from@example.com', ['to@example.com'],
-#             fail_silently=False,
-#         )
-#
-#         self.assertEqual(len(mail.outbox), 1)
-#
-#         self.assertEqual(mail.outbox[0].subject, 'Subject here')
-#         mail.outbox = []
+class StaffPageTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get(self):
+        response = self.client.get('/staff/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_post(self):
+        response = self.client.post('/staff/', {'login': 'admin', 'password': 'qwertY123'})
+        self.assertEquals(response.status_code, 200)
+
+
+class BookingPageTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_booking_post(self):
+        response = self.client.post('/accounts/login', {'username': 'tester', 'password': 'Jaxonyo23'})
+        self.assertEquals(response.status_code, 200)
+
+    def test_booking_get(self):
+        self.client.post('/accounts/login', {'username': 'tester', 'password': 'Jaxonyo23'})
+        response = self.client.get('/step_1/')
+        self.assertEquals(response.status_code, 302)
+
